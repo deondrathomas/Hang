19129df4d4f5a2956b8e5eaf90531b83445b7f3c
deondrathomas <48733047+deondrathomas@users.noreply.github.com>
2020-03-13 13:28:43 -0500

    Initial commit

---
+++ src/HangMan.java
@@ -0,0 +1,15 @@
+import javax.crypto.SecretKey;
+
+public class HangMan {
+    public static void main(String[] args) {
+        
+
+
+
+        System.out.println("Hey, Welcome To Hangman !!");
+        System.out.println("Good Luck :)");
+        System.out.println("Insert word here _ _ _ _ _ _ _");
+
+    }
+}
+
